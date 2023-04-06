8dcaccdb4bce1a4f0852bbbb47c96cb27e8c74ec
agggutie <aggutie@iu.edu>
2023-04-05 21:18:24 -0400

    start make divs

---
+++ labs/lab10/js/divs.js
@@ -0,0 +1,17 @@
+let dvOutput = document.getElementById("dvOutput");
+
+let objects = [
+
+    { color: "#FF0000", height: 100, width: 300 },
+   
+    { color: "#FFFF00", height: 200, width: 200 },
+   
+    { color: "#ff0000", height: 300, width: 100 },
+   
+   ];
+
+function create(){
+    for(var i = 0; i < objects.length; i++){
+        let newEl = document.createElement("div");
+    }
+}
